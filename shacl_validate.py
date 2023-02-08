941f73de86b7dcc2a4186d297e1a8ba68483bbf2
Kyle Lawlor-Bagcal <wgwz@users.noreply.github.com>
2023-02-08 09:47:55 -0500

    feat: project metadata separation (#37)
    * refactor: rename jsonld project files to simpler name
    
    * refactor: set up C01 project and project page shacl schema
    
    * refactor: set up C01 project and project page jsonld templates
    
    * refactor: rename and split C01 projects in ops into project and project page
    
    * feat: filter by class id option for shacl validate script
    
    * feat: add filter for credit class, project and credit batch to script
    
    * feat!: add single schema for project page and mark old project page schema as legacy
    
    * feat: adjustments to C01 and C02 project schema
    
    * refactor: clean up C01 project page ops data
    
    * refactor: split C02 project data into project and project page data
    
    * fix: correctly specify the constraints for the new project page schema
    
    * fix: specify only one project developer for C01 project schema
    
    * fix: move project developer into project data for C01
    
    * chore: remove project operator and add credit class id for C01 project page jsonld template
    
    * chore: clean up C02 project and project page jsonld templates
    
    * refactor: simplify jsonld structure for C01 project pages
    
    * fix: add conditional constraints for landStewardStory and landStewardStoryTitle
    
    * refactor: simplify jsonld structure for C01 projects
    
    * chore: update jsonld templates for new project and project page structure
    
    * refactor: convert C02 projects to use projectDeveloper instead of projectOperator
    
    * refactor: move VCS/CFC project page and VCS methodology into anchored schema
    
    * refactor: add JSON-LD id and remove vcs/cfc project id field in project page metadata
    
    * fix: remove desc field from C02 project template
    
    * refactor: CXX-XXX filename convention for projects
    
    * refactor: use Organization instead of OrganizationDisplay
    
    * fix: mark projectDeveloper as optional
    
    * fix: always use offsetGenerationMethod as a list
    
    * feat: add id field to C01/C02 projects

--- shacl_validate.py
+++ shacl_validate.py
@@ -1,20 +1,22 @@
 #!/usr/bin/env python3
+import argparse
 import glob
-from shutil import which
 import sys
-from subprocess import check_output, CalledProcessError, STDOUT
+from shutil import which
+from subprocess import STDOUT, CalledProcessError, check_output
 from tempfile import NamedTemporaryFile
 
 # If the script exits with a non-zero status code there was at least one failed
 # SHACL validation. This can be used to mark a CI check as failed.
 EXIT_CODE = 0
+CMD_SHACL_VALIDATE = "shacl validate --text --shapes={shapes} --data={data}"
 
 
 def shacl_validate(shapes, data):
     global EXIT_CODE
     try:
         result = check_output(
-            f"shacl validate --text --shapes={shapes} --data={data}".split(),
+            CMD_SHACL_VALIDATE.format(shapes=shapes, data=data).split(),
             encoding="utf-8",
             stderr=STDOUT,
         ).strip()
@@ -39,24 +41,39 @@ def sglob(pathname):
     return sorted(glob.iglob(pathname))
 
 
-def credit_class_validations(fp):
-    for data in sglob("*/*/credit-class-metadata/*.jsonld"):
+def credit_class_validations(fp, class_id=None):
+    if class_id is None:
+        glob_string = "*/*/credit-class-metadata/*.jsonld"
+    else:
+        glob_string = f"*/{class_id}/credit-class-metadata/*.jsonld"
+
+    for data in sglob(glob_string):
         shacl_validate(
             fp.name,
             data,
         )
 
 
-def project_validations(fp):
-    for data in sglob("*/*/project-metadata/*.jsonld"):
+def project_validations(fp, class_id=None):
+    if class_id is None:
+        glob_string = "*/*/project-metadata/*.jsonld"
+    else:
+        glob_string = f"*/{class_id}/project-metadata/*.jsonld"
+
+    for data in sglob(glob_string):
         shacl_validate(
             fp.name,
             data,
         )
 
 
-def credit_batch_validations(fp):
-    for data in sglob("*/*/credit-batch-metadata/*.jsonld"):
+def credit_batch_validations(fp, class_id=None):
+    if class_id is None:
+        glob_string = "*/*/credit-batch-metadata/*.jsonld"
+    else:
+        glob_string = f"*/{class_id}/credit-batch-metadata/*.jsonld"
+
+    for data in sglob(glob_string):
         shacl_validate(
             fp.name,
             data,
@@ -84,9 +101,64 @@ if __name__ == "__main__":
         print("For mac: https://formulae.brew.sh/formula/jena")
         print("Install Docs: https://jena.apache.org/download/index.cgi")
         sys.exit(1)
+
+    parser = argparse.ArgumentParser(
+        prog="shacl_validate",
+        description="A tool for validating SHACL schema against regen-registry-standards JSON-LD data graphs",
+    )
+    parser.add_argument(
+        "--build-schema-file-only",
+        help="Only build the schema file",
+        action="store_true",
+    )
+    parser.add_argument(
+        "--print-shacl-validate-cmd-only",
+        help="Only print the shacl validate cmd template string.",
+        action="store_true",
+    )
+    parser.add_argument(
+        "--credit-class-only",
+        help="Only validate credit class data",
+        action="store_true",
+    )
+    parser.add_argument(
+        "--project-only",
+        help="Only validate project data",
+        action="store_true",
+    )
+    parser.add_argument(
+        "--credit-batch-only",
+        help="Only validate credit batch data",
+        action="store_true",
+    )
+    parser.add_argument("--class-id", help="Only run validations for a particular class id")
+    args = parser.parse_args()
+
+    if args.build_schema_file_only:
+        with open("regen-registry-schema.ttl", "w") as fp:
+            build_schema_file(fp)
+        sys.exit(EXIT_CODE)
+
+    if args.print_shacl_validate_cmd_only:
+        print(CMD_SHACL_VALIDATE)
+        sys.exit(EXIT_CODE)
+
     with NamedTemporaryFile("w", suffix=".ttl") as fp:
         build_schema_file(fp)
-        credit_class_validations(fp)
-        project_validations(fp)
-        credit_batch_validations(fp)
-        sys.exit(EXIT_CODE)
+        
+        if args.credit_class_only:
+            credit_class_validations(fp, class_id=args.class_id)
+            sys.exit(EXIT_CODE)
+        if args.project_only:
+            project_validations(fp, class_id=args.class_id)
+            sys.exit(EXIT_CODE)
+        if args.credit_batch_only:
+            credit_batch_validations(fp, class_id=args.class_id)
+            sys.exit(EXIT_CODE)
+
+        # by default, validate all data
+        credit_class_validations(fp, class_id=args.class_id)
+        project_validations(fp, class_id=args.class_id)
+        credit_batch_validations(fp, class_id=args.class_id)
+
+    sys.exit(EXIT_CODE)
